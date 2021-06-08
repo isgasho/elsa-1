a64c837cede7f13530f4280a6e420121b3f8a63c
busgo <248434199@qq.com>
2021-06-08 14:57:41 +0800

    change log.go

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -96,36 +96,36 @@ func Fatal(args ...interface{}) {
 
 // Debugf uses fmt.Sprintf to log a templated message.
 func Debugf(template string, args ...interface{}) {
-	s.Debugf(template, args)
+	s.Debugf(template, args...)
 }
 
 // Infof uses fmt.Sprintf to log a templated message.
 func Infof(template string, args ...interface{}) {
-	s.Infof(template, args)
+	s.Infof(template, args...)
 }
 
 // Warnf uses fmt.Sprintf to log a templated message.
 func Warnf(template string, args ...interface{}) {
-	s.Warnf(template, args)
+	s.Warnf(template, args...)
 }
 
 // Errorf uses fmt.Sprintf to log a templated message.
 func Errorf(template string, args ...interface{}) {
-	s.Errorf(template, args)
+	s.Errorf(template, args...)
 }
 
 // DPanicf uses fmt.Sprintf to log a templated message. In development, the
 // logger then panics. (See DPanicLevel for details.)
 func DPanicf(template string, args ...interface{}) {
-	s.DPanicf(template, args)
+	s.DPanicf(template, args...)
 }
 
 // Panicf uses fmt.Sprintf to log a templated message, then panics.
 func Panicf(template string, args ...interface{}) {
-	s.Panicf(template, args)
+	s.Panicf(template, args...)
 }
 
 // Fatalf uses fmt.Sprintf to log a templated message, then calls os.Exit.
 func Fatalf(template string, args ...interface{}) {
-	s.Fatalf(template, args)
+	s.Fatalf(template, args...)
 }
